194f823283a264f6090bbf1f18df5b11d9f05a52
RyK57 <109132617+RyK57@users.noreply.github.com>
2023-04-17 20:57:07 +0530

    Update phylotrees.py

--- phylotrees.py
+++ phylotrees.py
@@ -14,7 +14,24 @@ import matplotlib
 
 matplotlib.use('TkAgg')
 
+[theme]
 
+# Primary accent for interactive elements
+primaryColor = '#7792E3'
+
+# Background color for the main content area
+backgroundColor = '#273346'
+
+# Background color for sidebar and most interactive widgets
+secondaryBackgroundColor = '#B9F1C0'
+
+# Color used for almost all text
+textColor = '#FFFFFF'
+
+# Font family for all text in the app, except code blocks
+# Accepted values (serif | sans serif | monospace) 
+# Default: "sans serif"
+font = "sans serif"
 
 def read_fasta(file):
     # Read the contents of the file
