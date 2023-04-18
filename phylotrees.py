3311034159a0af04306cb4fc1b1d2c100b8fb10f
RyK57 <109132617+RyK57@users.noreply.github.com>
2023-04-18 10:10:00 +0530

    Update phylotrees.py

--- phylotrees.py
+++ phylotrees.py
@@ -32,13 +32,13 @@ textColor = '#FFFFFF'
 font = "sans serif"
 
 def read_fasta(file):
-    # Read the contents of the file
+    # Reading the contents of the file
     contents = file.getvalue().decode("utf-8")
-    # Split the contents into lines
+    # Splitting the contents into lines
     lines = contents.split("\n")
-    # Remove any empty lines
+    # Removing any empty lines
     lines = [line for line in lines if line.strip()]
-    # Parse the FASTA sequences
+    # Parsing the FASTA sequences
     sequences = {}
     max_length = 0
     for line in lines:
@@ -48,77 +48,77 @@ def read_fasta(file):
         else:
             sequences[current_sequence] += line
 
-            # Keep track of the maximum sequence length
+            # Keeping track of the maximum sequence length
             if len(sequences[current_sequence]) > max_length:
                 max_length = len(sequences[current_sequence])
 
-    # Add padding to ensure all sequences are the same length
+    # Adding padding to ensure all sequences are the same length
     for sequence_name in sequences:
         sequence = sequences[sequence_name]
         padding = max_length - len(sequence)
         sequences[sequence_name] = sequence + "-" * padding
 
-    # Convert the sequences dictionary to a list of SeqRecord objects
+    # Converting the sequences dictionary to a list of SeqRecord objects
     seq_records = [SeqRecord(Seq(seq), id=name) for name, seq in sequences.items()]
-    # Align the sequences and return the result
+    # Aligning the sequences and return the result
     return align_sequences(seq_records)
 
 
 def align_sequences(seq_records):
-    # Use the first sequence as the template for the alignment
+    # Using the first sequence as the template for the alignment
     template_seq = seq_records[0].seq
-    # Create a list of sequences to be aligned
+    # Creating a list of sequences to be aligned
     seqs_to_align = [str(seq_record.seq) for seq_record in seq_records]
-    # Align the sequences
+    # Aligning the sequences
     alignment = MultipleSeqAlignment(seq_records)
-    # Return the alignment
+    # Returning the alignment
     return alignment
 
 def build_phylogenetic_tree(alignment):
-    # Create a distance calculator object
+    # Creating a distance calculator object
     calculator = DistanceCalculator('identity')
-    # Calculate the distances between sequences in the alignment
+    # Calculating the distances between sequences in the alignment
     distance_matrix = calculator.get_distance(alignment)
-    # Create a distance-based tree constructor object
+    # Creating a distance-based tree constructor object
     constructor = DistanceTreeConstructor()
-    # Build the tree
+    # Buildng the tree
     tree = constructor.upgma(distance_matrix)
     return tree
 
-# # Set page title
+# # Setting page title
 # st.set_page_config(page_title="FASTA File Upload")
 
-# Create upload button
+# Upload button
 uploaded_file = st.file_uploader(
     "Upload a text file containing FASTA sequences", type=["txt"]
 )
 
 file_url = 'https://drive.google.com/file/d/1vwHW4NcsU6tfJPbKVr1CGiZLtz5NpgtY/view?usp=sharing'
 
-# Create a link to download the file
+# link to download the example sequence file
 st.markdown(f'<a href="https://drive.google.com/file/d/1vwHW4NcsU6tfJPbKVr1CGiZLtz5NpgtY/view?usp=sharing" download>Example Input file (M19961 Accession Number NCBI)</a>', unsafe_allow_html=True)
 
-# If file is uploaded, read and display alignment
+# If file is uploaded, reading and displaying alignment
 if uploaded_file is not None:
     alignment = read_fasta(uploaded_file)
     summary_align = AlignInfo.SummaryInfo(alignment)
     consensus = summary_align.dumb_consensus()
     st.write(f"Consensus sequence:\n{consensus}")
     st.write("Aligned sequences:")
-    # Set maximum alignment length to display
+    # Setting maximum alignment length to display
     MAX_ALIGNMENT_LENGTH = 1000
 
-    # Print pairwise alignment for each sequence
+    # Printing pairwise alignment for each sequence
     for seq_record in alignment:
         st.write(seq_record.id)
         st.write(seq_record.seq)
         st.write("")
 
-        # Align the sequence with the consensus sequence
+        # Aligning the sequence with the consensus sequence
         pairwise_alignments = align_sequences([seq_record, SeqRecord(Seq(str(consensus)), id="Consensus")])
 
 
-        # Print pairwise alignment for each pair of sequences
+        # Printing pairwise alignment for each pair of sequences
         for a in pairwise_alignments:
             # Check if alignment is shorter than the maximum length
             if len(a[0]) <= MAX_ALIGNMENT_LENGTH and len(a[1]) <= MAX_ALIGNMENT_LENGTH and len(a[2]) <= MAX_ALIGNMENT_LENGTH:
@@ -126,17 +126,21 @@ if uploaded_file is not None:
                 alignment_str = format_alignment(*alignments[0], full_sequences=False)
                 st.write(alignment_str)
 
-    # Calculate distance matrix
+    # Calculating distance matrix
     calculator = DistanceCalculator("identity")
     distance_matrix = calculator.get_distance(alignment)
     st.write("Distance matrix:")
     st.write(distance_matrix)
 
-    # Construct the tree
+    # Constructing the tree
     constructor = DistanceTreeConstructor(calculator)
     tree = constructor.build_tree(alignment)
     st.write("Phylogenetic tree:")
     st.write(tree)
 
-    # Draw the tree
+    # Drawing the tree
     Phylo.draw(tree)
+    
+    plot = matplotlib.pyplot.gcf()
+    # Displaying the plot in Streamlit ap
+    st.pyplot(plot)
